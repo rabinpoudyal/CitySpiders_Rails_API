0ef914b8486a27c2e72e3b2cba9b6acdc64d23af
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2017-05-06 06:33:51 +0545

    Finished with books controller

--- app/controllers/v1/books_controller.rb
+++ app/controllers/v1/books_controller.rb
@@ -1,23 +1,45 @@
 class V1::BooksController < ApplicationController
+  before_action :set_book, only: [:show,:update,:destroy]
+  
   def index
     @books = Book.all
     render json: @books, status: :ok
   end
 
+  def show
+    render json: @book
+  end
+
   def create
     @book = Book.new(book_params)
     if @book.save
-      head(:ok)
+      render json: @book, status: :ok
     else
-      head(:sorry)
+      render json: @book.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-
+    if @book.destroy
+      head(:ok)
+    else
+      head(:unprocessable_entity)
+    end
   end
 
+  def update
+    if @book.update(book_params)
+      render json: @book
+    else
+      render json: @book.errors, status: :unprocessable_entity
+    end
+
   private
+
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
     def book_params
       params.require(:book).permit(:title,:isbn,:price,:condition,:location,:category,:is_sold)
     end
