042a927458126074052a4d70e16f5f5c12db9c03
Rabin Poudyal <rabinpoudyal1995@gmail.com>
2017-05-08 10:17:55 +0545

    Set up association between user and book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  belongs_to :user
+  validates_presence_of :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   acts_as_token_authenticatable
+  has_many :books, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def full_name
+    "#{f_name}" + "#{l_name}"
+  end
 end
